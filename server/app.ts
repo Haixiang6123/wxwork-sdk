bbb03f1187e27fe3f51744a08fa05aa4784e0128
Haixiang6123 <haixiang6123@gmail.com>
2021-01-08 01:12:07 +0800

    feat: 添加缓存读取

--- server/app.ts
+++ server/app.ts
@@ -5,20 +5,23 @@ import * as path from 'path';
 import axios from 'axios';
 import routes from './routes';
 import {GetTokenRes} from './types';
+import dayjs = require('dayjs');
 
 dotenv.config();
 
 const app = express();
 const port = 1234;
-const cacheUrl = path.join(__dirname, './cache');
+const cacheUrl = path.join(__dirname, './cache.txt');
 
-let accessToken: string | null = null;
+let accessToken: string | null;
+let expires: string | null;
+
+[accessToken, expires] = fs.readFileSync(cacheUrl, 'utf8').split('\n');
 
 app.get('/gettoken', async (req, res) => {
-  const cache = fs.readFileSync(cacheUrl, 'utf8');
-
-  if (cache) {
-    return res.json({ code: 0, msg: '' });
+  // 未过期
+  if (expires && dayjs().diff(dayjs(expires)) < 0) {
+    return res.json({ code: 0, msg: '缓存中读取' });
   }
 
   const params = {
@@ -31,12 +34,23 @@ app.get('/gettoken', async (req, res) => {
     params,
   })
 
-  accessToken = response.data.access_token || null;
+  const { access_token, expires_in, errmsg } = response.data;
 
-  fs.writeFileSync(cacheUrl, accessToken, 'utf8');
+  if (errmsg) {
+    return res.json({ code: 500, msg: '获取失败' });
+  }
 
-  return res.json({ code: 0, msg: '' });
-})
+  // 计算过期时间
+  const curtExpires = dayjs().add(expires_in, 'second');
+  // 存入缓存中
+  [accessToken, expires] = [access_token, curtExpires.toISOString()];
+  // 写入文件里
+  const cacheText = access_token + '\n' + curtExpires.toISOString();
+
+  fs.writeFileSync(cacheUrl, cacheText, 'utf8');
+
+  return res.json({ code: 0, msg: '企业微信中获取' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
